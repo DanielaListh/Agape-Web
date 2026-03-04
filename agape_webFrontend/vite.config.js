296af7ef5c21126c9af4c0dc325e2e1a2da360cb
DanielaListh <dani_moe_berrios@hotmail.com>
2026-03-04 16:40:26 -0400

    corrección de typo en comentario en vite.config.js

--- agape_webFrontend/vite.config.js
+++ agape_webFrontend/vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   plugins: [react()],
 })
 
-// el root es importante, porque me ayuda a seber desde que carpeta va a tomar los archivos que
+// el root es importante, porque me ayuda a saber desde que carpeta va a tomar los archivos que
 // va a autiliozar para generar el dist/ de la build
 // esto le dice a vite que compile desde agape_webFrontend y genere el build en dist/
 
